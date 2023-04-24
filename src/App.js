1b770ab3dfb0affb6d03e3f630ef3305b7f5fb46
Tony W <bobacrypto2@gmail.com>
2023-04-23 19:44:35 -0700

    Create handleInput function for components to handle onChange events to update our states correctly

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import React, { useState, useEffect } from "react";
+import './styles/App.css'
+import './styles/Forms.css'
+import './styles/CVApp.css'
+import General from "./components/General";
 
 const App = () => {
-  const [general, setGeneral] = useState([{
+  const [general, setGeneral] = useState({
     name: {text: '', edit: ''},
     email: {text: '', edit: ''},
     phone: {text: '', edit: ''}
-  }])
+  })
 
-  const [education, setEducation] = useState([{
+  const [education, setEducation] = useState({
     school: {text: '', edit: ''},
     study: {text: '', edit: ''},
     date: {start: '', end: '', edit: ''}
-  }])
+  })
 
-  const [practical, setPractical] = useState([{
+  const [practical, setPractical] = useState({
     company: {text: '', edit: ''},
     position: {text: '', edit: ''},
     skills: {start: '', end: '', edit: ''}
-  }])
+  })
+
+  function testObject() {
+    console.log(general)
+  }
+
+  function handleInput(value, id) {
+    setGeneral((existingValues) => ({...existingValues, [id]: {text: value, edit: ''}}))
+  }
+
+  return (
+    <div className="main">
+      <div className="cv-forms">
+        <General general={general} onChange={handleInput}/>
+        <button id='submit-cv'>Submit</button>
+        <button id='edit-cv'>Edit</button>
+      </div>
+      <button onClick={testObject}>Test</button>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/General.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+const General = (props) => {
+
+    function handleChange(e) {
+        props.onChange(e.target.value, e.target.dataset.id)
+    }
+
+    return (
+        <div className='general-info'>
+            <form>
+                <label htmlFor="nameInput">Name</label>
+                <input type="text" id="nameInput" data-id="name" 
+                placeholder="John Doe" value={props.general.name.text} onChange={handleChange}></input>
+
+                <label htmlFor="emailInput">Email</label>
+                <input type="email" id="emailInput" data-id="email" 
+                placeholder="johndoe@gmail.com" value={props.general.email.text} onChange={handleChange}></input>
+
+                <label htmlFor="phoneInput">Phone #</label>
+                <input type="tel" id="phoneInput" data-id="phone" 
+                placeholder="000-000-0000" value={props.general.phone.text} onChange={handleChange}></input>
+            </form>
+        </div>
+        
+    )
+}
+
+export default General;
